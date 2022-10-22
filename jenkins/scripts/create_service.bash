eae1c44ab1035bac265f78aa4c023fe67d97d4af
Ranjit Naik <ranjit.naik@gmail.com>
2022-10-22 17:41:20 +0100

    perm given

---
+++ jenkins/scripts/create_service.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+SERVICE_NAME=$1
+SERVICE_YAML=$2
+
+OUTPUT=`kubectl get service 2>/dev/null | grep ${SERVICE_NAME}`
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo "Kubernetes service is already present. No action needed."
+else
+    echo "Kubernetes service does not exist. Creating new secret"
+    kubectl create service -f ${SERVICE_YAML}
+fi
