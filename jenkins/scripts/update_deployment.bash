844a6f1e9b57f1a9a6e77afa60d58888de633ec7
Ranjit Naik <ranjit.naik@gmail.com>
2022-10-22 17:30:00 +0100

    perm given

--- jenkins/scripts/update_deployment.bash
+++ jenkins/scripts/update_deployment.bash
@@ -6,6 +6,19 @@ DEPLOYMENT_YAML=$2
 CONTAINER=$3
 IMAGE=$4
 IMAGE_TAG=$5
+SECRET_NAME=$6
+JFROG_REPO=$7
+JFROG_USR=$8
+JFROG_PSW=$9
+
+OUTPUT=`kubectl get secrets 2>/dev/null | grep $SECRET_NAME`
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo "Kubernetes docker secret is already present. No action needed."
+else
+    echo "Kubernetes docker secret does not exist. Creating new secret"
+    kubectl create secret docker-registry regcred --docker-server=${JFROG_REPO} --docker-username=${$JFROG_USR} --docker-password=${JFROG_PSW} --docker-email=${$JFROG_USR}
+fi
 
 OUTPUT=`kubectl get deployments 2>/dev/null | grep $DEPLOYMENT`
 RESULT=$?
